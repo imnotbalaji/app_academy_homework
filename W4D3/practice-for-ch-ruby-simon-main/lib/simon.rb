ad35642eab7160bba4b4c23a08870c71eeba8754
Balaji <imnotbalaji@gmail.com>
2023-09-13 08:06:50 -0400

    making more changes to gameplay

--- W4D3/practice-for-ch-ruby-simon-main/lib/simon.rb
+++ W4D3/practice-for-ch-ruby-simon-main/lib/simon.rb
@@ -4,21 +4,39 @@ class Simon
   attr_accessor :sequence_length, :game_over, :seq
 
   def initialize
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
   end
 
   def play
+    
   end
 
   def take_turn
+ 
+    if show_sequence == require_sequence
+      round_success_message
+      self.sequence_length = sequence_length + 1
+      return true 
+    else 
+      return false
+    end 
   end
 
   def show_sequence
+    add_random_color
+    seq
   end
 
   def require_sequence
+    input = gets.chomp
+    input.split(" ")
   end
 
   def add_random_color
+   seq << %w(red blue yellow green).sample 
+   
   end
 
   def round_success_message
