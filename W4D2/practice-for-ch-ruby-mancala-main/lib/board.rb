d741fa7ad664f00830de778cd2e02324e23c7b9a
Balaji <imnotbalaji@gmail.com>
2023-09-14 09:13:19 -0400

    starting off the W4D4 homework

--- W4D2/practice-for-ch-ruby-mancala-main/lib/board.rb
+++ W4D2/practice-for-ch-ruby-mancala-main/lib/board.rb
@@ -1,16 +1,18 @@
+require "byebug"
 class Board
   attr_accessor :cups
 
   def initialize(name1, name2)
-    @cups = Array.new(14) {Array.new(0)}
-    @cups.each_with_index do |cup,index|
-       if index != 6 && index !=13
-        4.times { cup << :stone}
-       end 
-    end 
+    @cups = Array.new(14) {Array.new()}
+    place_stones
   end
 
   def place_stones
+    cups.each_with_index do |cup,index|
+      if index != 6 && index !=13
+       4.times { cup << :stone}
+      end 
+   end 
     # helper method to #initialize every non-store cup with four stones each
   end
 
@@ -27,7 +29,24 @@ class Board
   end
 
   def make_move(start_pos, current_player_name)
-    cups[start_pos] = []
+    # debugger
+    # start_index = (1..6).include?(start_pos) ? start_pos - 1 : start_pos
+    opposite_player_cup_index = ((0..5).include?(start_pos) ?  13 : 6 )
+    # debugger if (0..5).include?(start_pos) 
+    
+    stones_in_hand = cups[start_pos].dup
+    self.cups[start_pos] = []
+    next_pos = (start_pos +1)%14
+    until stones_in_hand.empty?
+      if next_pos != opposite_player_cup_index
+          stone  = stones_in_hand.pop
+          cups[next_pos].push(stone)
+      end 
+      next_pos = (next_pos +1)%14
+     
+    end  
+
+
   end
 
   def next_turn(ending_cup_idx)
